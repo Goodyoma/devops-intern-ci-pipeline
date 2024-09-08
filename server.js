207a614925f9370df4456c82ebf70b1505dbdc3c
Goodness Mark <159780715+Goodyoma@users.noreply.github.com>
2024-09-08 12:35:42 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Define the port
+const PORT = 80;
+
+// Define a route to respond with a message
+app.get('/', (req, res) => {
+  res.send('Welcome to, Balancee!'); 
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
